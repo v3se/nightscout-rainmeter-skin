11e3143a42bb9e32ab2de49166cbabc97add16c5
Robert Vesterinen <worsethanbullets@gmail.com>
2023-09-30 14:38:16 +0300

    Removed delta from the skin since LibreLinkUp updater does not support it

--- TrendArrow.lua
+++ TrendArrow.lua
@@ -1,51 +1,15 @@
 function Update()
     arrows = {
-        [-1] = "[\\11143]",
-        [-0.9] = "[\\11143]",
-        [-0.8] = "[\\11143]",
-        [-0.7] = "[\\11112]",
-        [-0.6] = "[\\11112]",
-        [-0.5] = "[\\11112]",
-        [-0.4] = "[\\11112]",
-        [-0.3] = "[\\11106]",
-        [-0.2] = "[\\11106]",
-        [-0.1] = "[\\11106]",
-        [0] = "[\\11106]",
-        [0.1] = "[\\11106]",
-        [0.2] = "[\\11106]",
-        [0.3] = "[\\11106]",
-        [0.4] = "[\\11111]",
-        [0.5] = "[\\11111]",
-        [0.6] = "[\\11111]",
-        [0.7] = "[\\11111]",
-        [0.8] = "[\\11141]",
-        [0.9] = "[\\11141]",
-        [1] = "[\\11141]",
+        ['Flat'] = "[\\11106]",
+        ['SingleUp'] = "[\\11141]",
+        ['SingleDown'] = "[\\11143]",
+        ['FortyFiveUp'] = "[\\11111]",
+        ['FortyFiveDown'] = "[\\11112]",
     }
-    MeasureObject = SKIN:GetMeasure('MeasureBGDelta')
-	MeasureBGDelta = round(MeasureObject:GetValue(), 1)
-    -- print('Rounded Glucose: ' .. MeasureBGDelta)
+    MeasureObject = SKIN:GetMeasure('SGVParserBGArrow')
+    print(MeasureObject:GetStringValue())
     -- print(MeasureObject:GetValue())
-    if MeasureBGDelta > 1
-    then
-        SKIN:Bang('!SetOption', 'MeterArrow', 'Text', '[\\11141]')
-    elseif MeasureBGDelta < -1
-    then
-        SKIN:Bang('!SetOption', 'MeterArrow', 'Text', '[\\11143]')
-    else
-        print(arrows[MeasureBGDelta])
-        SKIN:Bang('!SetOption', 'MeterArrow', 'Text', arrows[MeasureBGDelta])
-    end
 
-    if MeasureBGDelta > 0
-    then
-        SKIN:Bang('!SetOption', 'MeterBGDelta', 'Text', '+' .. MeasureBGDelta)
-    else
-        SKIN:Bang('!SetOption', 'MeterBGDelta', 'Text', MeasureBGDelta)
-    end
-end
+    SKIN:Bang('!SetOption', 'MeterArrow', 'Text', arrows[MeasureObject:GetStringValue()])
 
-function round(num, numDecimalPlaces)
-    local mult = 10^(numDecimalPlaces or 0)
-    return math.floor(num * mult + 0.5) / mult
 end
